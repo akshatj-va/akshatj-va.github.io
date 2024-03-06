fde694e8fe1215eae451049015298f616569b44f
akshatj-va <156286866+akshatj-va@users.noreply.github.com>
2024-03-06 13:20:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 function initEmbeddedMessaging() {
-    console.log('inside initEmbeddedMessaging');
-		try {
-			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+	console.log('from main.js file');
+    	console.log('inside initEmbeddedMessaging');
+	try {
+		embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
 
-			embeddedservice_bootstrap.init(
-				'00DEm0000010wVl',
-				'VA_Web_Chat_Github',
-				'https://visiblealpha--full.sandbox.my.site.com/ESWVAWebChatGithub1709634412936',
-				{
-					scrt2URL: 'https://visiblealpha--full.sandbox.my.salesforce-scrt.com'
-				}
-			);
-		} catch (err) {
-			console.error('Error loading Embedded Messaging: ', err);
-		}
+		embeddedservice_bootstrap.init(
+			'00DEm0000010wVl',
+			'VA_Web_Chat_Github',
+			'https://visiblealpha--full.sandbox.my.site.com/ESWVAWebChatGithub1709634412936',
+			{
+				scrt2URL: 'https://visiblealpha--full.sandbox.my.salesforce-scrt.com'
+			}
+		);
+	} catch (err) {
+		console.error('Error loading Embedded Messaging: ', err);
+	}
 };
